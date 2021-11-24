788bdc3445db68c566f46cf7b13b7fa8db0e9f70
Zikai Xie <zikaix@liverpool.ac.uk>
2021-11-24 11:31:26 +0000

    in construction - 60%

--- base_optimizer.py
+++ base_optimizer.py
@@ -1,16 +1,18 @@
 from bayes_opt import bayesian_optimization as bo
+from bayes_opt.event import Events
 import gpytorch
 import torch
 from gpytorch_models.linear_weight_GP import ExactGPModel
+from utils import gpytorch_acq_max, train_gp_model
 import warnings
 
 
-class gpytorch_optimizer(bo.BayesianOptimization):
+class GpytorchOptimization(bo.BayesianOptimization):
 
     def __init__(self, f, pbounds, random_state=None, verbose=2,
                  bounds_transformer=None, gp_regressor=None, GP=ExactGPModel):
-        super(gpytorch_optimizer, self).__init__(f, pbounds=pbounds, random_state=random_state, verbose=verbose,
-                                                 bounds_transformer=bounds_transformer)
+        super(GpytorchOptimization, self).__init__(f, pbounds=pbounds, random_state=random_state, verbose=verbose,
+                                                   bounds_transformer=bounds_transformer)
         if gp_regressor is None:
             self._GP = GP
             self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
@@ -21,4 +23,27 @@ class gpytorch_optimizer(bo.BayesianOptimization):
         gtorch_model = self._GP(torch.Tensor(self.space.params),
                                 torch.Tensor(self.space.target),
                                 self.likelihood)
+        train_gp_model(gtorch_model, epochs=50, verbose=0)
+        suggestion = gpytorch_acq_max(
+            ac=utility_function.utility,
+            gp=self._gp,
+            y_max=self._space.target.max(),
+            bounds=self._space.bounds,
+            random_state=self._random_state
+        )
+        return self._space.array_to_params(suggestion)
 
+    def maximize(self,
+                 init_points=5,
+                 n_iter=25,
+                 acq='ucb',
+                 kappa=2.576,
+                 kappa_decay=1,
+                 kappa_decay_delay=0,
+                 xi=0.0,
+                 **gp_params):
+        """Mazimize your function"""
+        self._prime_subscriptions()
+        self.dispatch(Events.OPTIMIZATION_START)
+        self._prime_queue(init_points)
+        self.set_gp_params(**gp_params)
--- utils.py
+++ utils.py
@@ -1,39 +1,10 @@
-import torch
 import gpytorch
 import numpy as np
 import bayes_opt
-
-
-'''def train_gp_model(gp_model, train_x, train_y, optimizer=None, epochs=100, mll=None, verbose=1):
-    """
-    Training procedure of Gaussian process model.
-    :param gp_model: GP model to be trained
-    :param train_x: Training data
-    :param train_y: Training label
-    :param optimizer: Training optimizer. If not provided, will use torch.optim.Adam
-    :param epochs: Training epochs, default is 10
-    :param mll: Marginal likelihood for the GP model, default is gpytorch.mlls.ExactMarginalLogLikelihood
-    :param verbose: Controller of showing training procedure
-    :return: NA
-    """
-    gp_model.train()
-    gp_model.likelihood.train()
-    if optimizer is None:
-        optimizer = torch.optim.Adam(gp_model.parameters(), lr=0.1)
-    if mll is None:
-        mll = gpytorch.mlls.ExactMarginalLogLikelihood(gp_model.likelihood, gp_model)
-
-    for i in range(epochs):
-        optimizer.zero_grad()
-        output = gp_model(train_x)
-        loss = -mll(output, train_y)
-        loss.backward()
-        if verbose == 1:
-            print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
-                i + 1, epochs, loss.item(),
-                gp_model.covar_module.base_kernel.lengthscale.item(),
-                gp_model.likelihood.noise.item()))
-        optimizer.step()'''
+from scipy.optimize import minimize
+from bayes_opt.util import UtilityFunction
+import torch
+from scipy.stats import norm
 
 
 def train_gp_model(gp_model, optimizer=None, epochs=100, mll=None, verbose=1):
@@ -96,3 +67,70 @@ def get_mean_variance(gp_model, x):
     f_var = f_predict.variance
     f_covar = f_predict.covariance_matrix
     return f_mean, f_var, y_predict, f_covar
+
+
+class GpytorchUtilityFunction(UtilityFunction):
+
+    def __init__(self,  kind, kappa, xi, kappa_decay=1, kappa_decay_delay=0):
+        super(GpytorchUtilityFunction, self).__init__(kind, kappa, xi, kappa_decay, kappa_decay_delay)
+
+    @staticmethod
+    def _ucb(x, gp, kappa):
+        gp.eval()
+        gp.likelihood.eval()
+        mean, var, _, _ = get_mean_variance(gp, torch.Tensor(x))
+        std = torch.sqrt(var)
+        ucb_value = mean + kappa * std
+        return ucb_value.numpy()
+
+    @staticmethod
+    def _ei(x, gp, y_max, xi):
+        gp.eval()
+        gp.likelihood.eval()
+        mean, var, _, _ = get_mean_variance(gp, torch.Tensor(x))
+        std = torch.sqrt(var)
+        a = (mean - y_max - xi).numpy()
+        z = (a / std).numpy()
+        return a * norm.cdf(z) + std * norm.pdf(z)
+
+    @staticmethod
+    def _poi(x, gp, y_max, xi):
+        gp.eval()
+        gp.likelihood.eval()
+        mean, var, _, _ = get_mean_variance(gp, torch.Tensor(x))
+        std = torch.sqrt(var)
+        z = ((mean - y_max - xi) / std).numpy()
+        return norm.cdf(z)
+
+
+def gpytorch_acq_max(ac, gp, y_max, bounds, random_state, n_warmup=10000, n_iter=10):
+    # Warm up with random points
+    x_tries = random_state.uniform(bounds[:, 0], bounds[:, 1],
+                                   size=(n_warmup, bounds.shape[0]))
+
+    ys = ac(x_tries, gp=gp, y_max=y_max)
+    x_max = x_tries[ys.argmax()]
+    max_acq = ys.max()
+
+    # Explore the parameter space more throughly
+    x_seeds = random_state.uniform(bounds[:, 0], bounds[:, 1],
+                                   size=(n_iter, bounds.shape[0]))
+    for x_try in x_seeds:
+        # Find the minimum of minus the acquisition function
+        res = minimize(lambda x: -ac(x.reshape(1, -1), gp=gp, y_max=y_max),
+                       x_try.reshape(1, -1),
+                       bounds=bounds,
+                       method="L-BFGS-B")
+
+        # See if success
+        if not res.success:
+            continue
+
+        # Store it if better than previous minimum(maximum).
+        if max_acq is None or -res.fun[0] >= max_acq:
+            x_max = res.x
+            max_acq = -res.fun[0]
+
+    # Clip output to make sure it lies within the bounds. Due to floating
+    # point technicalities this is not always the case.
+    return np.clip(x_max, bounds[:, 0], bounds[:, 1])
